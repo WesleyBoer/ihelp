7509a5384a79a1591b17d49f5f29f89324b5caaa
Wesley Boer <wesleysboer@gmail.com>
2022-06-12 21:00:29 -0300

    Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuFooterComponent } from './menu-footer/menu-footer.component';
+import { MaterialModule } from './shared/modules/material/material.module';
+import { HeaderHomeComponent } from './header-home/header-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuFooterComponent,
+    HeaderHomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
